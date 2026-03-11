36ad390dc9e219b772393db1ea6cf6d225bd7e7d
HaniefuL <haniefulgeofarry@gmail.com>
2026-03-12 05:55:10 +0700

    menambahkan KtpController

--- src/main/java/com/pertemuan3/ktpserver/controller/KtpController.java
+++ src/main/java/com/pertemuan3/ktpserver/controller/KtpController.java
@@ -1,2 +1,13 @@
 package com.pertemuan3.ktpserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/ktp")
+public class KtpController {
+
+    @Autowired
+    private KtpService ktpService;
+}
